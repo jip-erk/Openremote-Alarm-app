c8476c6de30c8835521106c61543c8861cc2bd6d
JornJongstra <94459310+JornJongstra@users.noreply.github.com>
2025-10-06 18:33:28 +0200

    Revert "View alarm details"
    This reverts commit c5b9f5a9d42ec172bd3d40ec977ee3bf90b7d197.

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,13 @@
   import Header from "./lib/Header.svelte";
   import AlarmCard from "./lib/Alarm.svelte";
   import AddAlarmForm from "./lib/AddAlarmForm.svelte";
-  import ViewAlarmForm from "./lib/ViewAlarmForm.svelte";
   import openremote from "@openremote/core";
-  import type { Auth, AlarmEvent, SentAlarm, Alarm} from "@openremote/model";
+  import type { Auth, AlarmEvent, SentAlarm, Alarm } from "@openremote/model";
   import rest from "@openremote/rest";
 
   let initialized = $state(false);
   let alarms = $state<SentAlarm[]>([]);
-  // let alarm = $state<SentAlarm>();
-  let alarmView = $state<SentAlarm>();
   let addPage = $state(false);
-  let viewPage = $state(false);
-
   openremote
     .init({
       managerUrl: "https://localhost",
@@ -43,7 +38,6 @@
     const response = await rest.api.AlarmResource.getAlarms({
       realm: "master",
     });
-    console.log(response);
     alarms = response.data;
   };
 
@@ -52,16 +46,6 @@
     console.log("Alarm created:", response.data);
     addPage = false;
   };
-
-  // const fetchAlarmById = async (alarmId: number) => {
-  //   const response = await rest.api.AlarmResource.getAlarm(alarmId);
-  //   alarm = response.data;
-  //   console.log("Alarm", alarm);
-  // }
-
-    const fetchAlarmById = async (alarm: SentAlarm) => {
-
-  }
 </script>
 
 <main>
@@ -73,35 +57,13 @@
       >{addPage ? "Cancel adding" : "Add"} alarm</button
     >
   </div>
-  {#if viewPage}
-    <div class="px-4">
-    <button
-      class="p-2 bg-primary/20 ring-1 w-full rounded-md ring-primary"
-      onclick={() => (viewPage = !viewPage)}>cancel view alarm</button
-    >
-    </div>
-  {/if}
-    <!-- <div class="px-4">
-    <button
-      class="p-2 bg-primary/20 ring-1 w-full rounded-md ring-primary"
-      onclick={() => (viewPage = !viewPage)}
-      >{viewPage ? "Cancel view" : "View"} alarm</button
-    >
-  </div> -->
   {#if addPage}
     <AddAlarmForm {addAlarm} />
-  {:else if viewPage}
-    <ViewAlarmForm {alarmView} />
   {:else}
     <div class="p-4 flex flex-col gap-4">
       {#if initialized}
         {#each alarms as alarm}
-          <!-- <button onclick="{() => (viewPage = !viewPage, fetchAlarmById(alarm.id))}">
-            <AlarmCard {alarm} />
-          </button> -->
-          <button onclick="{() => (viewPage = !viewPage, alarmView = alarm)}">
-            <AlarmCard {alarm} />
-          </button>
+          <AlarmCard {alarm} />
         {/each}
       {/if}
     </div>
